4fc9154e4b005f15cac017c6f5b338b0e7eb4be0
0xjoshcarter <MorgiaSharan860@hotmail.com>
2025-10-20 21:59:05 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,17 +1,24 @@
+import * as dotenv from "dotenv";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+dotenv.config();
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedVault = await deploy("PrivateBankVault", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  if (!process.env.INFURA_API_KEY) {
+    console.warn("INFURA_API_KEY is not set in environment variables");
+  }
+
+  console.log(`PrivateBankVault contract: `, deployedVault.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_private_bank_vault"; // id required to prevent reexecution
+func.tags = ["PrivateBankVault"];
